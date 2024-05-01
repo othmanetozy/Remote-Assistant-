c42b96087bcdc30e1be36eca5545c4bec23adc00
othmanetozy <tozyothmane123@gmail.com>
2024-05-01 20:21:55 +0100

    adding simple logic

--- Users/src/main/java/berger/levrault/app/users/Repo/RoomRepo.java
+++
@@ -1,7 +0,0 @@
-package berger.levrault.app.users.Repo;
-
-import berger.levrault.app.users.Entity.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepo extends JpaRepository<Room, Integer> {
-}
