d19e4b24a20c2aba310fb78cffa8ac424b5166c5
othmanetozy <tozyothmane123@gmail.com>
2024-05-03 16:41:30 +0100

    adding UI

--- Users/src/main/java/berger/levrault/users/Controllers/UserController.java
+++ Users/src/main/java/berger/levrault/users/Controllers/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping
 public class UserController {
 
     @Autowired
