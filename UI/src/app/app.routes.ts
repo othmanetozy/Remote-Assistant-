797c39d3f63b24bfac228e173199f2e9fffa2988
othmane tozy <tozyothmane123@gmail.com>
2024-05-06 11:40:24 +0100

    update 2 login

--- UI/src/app/app.routes.ts
+++ UI/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import {WelcomeComponent} from "./welcome/welcome.component";
-import {LoginComponent} from "./login/login.component";
+import {LoginComponent} from "../authentification/login/login.component";
 
 export const routes: Routes = [
   { path: '', component: WelcomeComponent},
